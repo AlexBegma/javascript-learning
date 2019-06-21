b63c747421c3341f4d8019abe98f994939e946fa
alexbehma <alexbegma@gmail.com>
2019-06-21 22:33:21 +0300

    task 21.06

--- js/main.js
+++ js/main.js
@@ -1,29 +1,9 @@
 "use strict";
 
-var message = 'I love JavaScript!';
-alert(message);
 
-// Объявите две переменные: admin и name.
-// Запишите в name строку "Василий".
-// Скопируйте значение из name в admin.
-// Выведите admin (должно вывести «Василий»).
-var name = 'Vasiliy';
-var admin = name;
-alert(admin);
-
-
-// Напишите функцию isInteger(num), которая возвращает true, если num – целое число, иначе false.
-function isInteger(num) {
-	return (num ^ 0) === num;
-}
-alert( isInteger(1) );
-alert( isInteger(1.5) );
-alert( isInteger(-0.5) );
-
-
-let question = confirm('What is your name?', '');
-alert(question);
-let question2 = prompt('What is your name?', '');
-alert(question2);
-
-
+let str = "abcde";
+alert(str[0]);
+alert(str[1]);
+alert(str[2]);
+alert(str[3]);
+alert(str[4]);
