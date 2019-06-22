bc5895175d93153505e223c38ebffe45ee6479ed
alexbehma <alexbegma@gmail.com>
2019-06-22 21:32:06 +0300

    task 2

--- task-2206/main.js
+++ task-2206/main.js
@@ -1,3 +1,12 @@
 let arr = ['Привет', ' мир ','!'];
 arr[0] = 'Пока';
 alert(arr);
+
+let payment = {
+	'Vano':'1200 grn',
+	'John':'4000 grn',
+	'Mikle':'3400 grn'
+};
+alert(payment['Vano']);
+alert(payment['John']);
+alert(payment['Mikle']);
